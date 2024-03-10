5e5def42fad61a37641d58466baeb6e2fc61f3a3
Dan <38832025+dayanch96@users.noreply.github.com>
2024-03-10 07:04:44 +0300

    YTAlertView

--- Source/Prefs/YTMDownloads.h
+++ Source/Prefs/YTMDownloads.h
@@ -9,3 +9,12 @@
 @property (nonatomic, strong) UIImageView *imageView;
 @property (nonatomic, strong) UILabel *label;
 @end
+
+@interface YTAlertView : UIView
+@property (nonatomic, copy, readwrite) UIImage *icon;
+@property (nonatomic, copy, readwrite) NSString *title;
+@property (nonatomic, copy, readwrite) NSString *subtitle;
++ (instancetype)infoDialog;
++ (instancetype)confirmationDialogWithAction:(void (^)(void))action actionTitle:(NSString *)actionTitle;
+- (void)show;
+@end
