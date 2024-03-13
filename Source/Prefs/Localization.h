a5707d9a65e6d22120bc17de3b461d885e1cd8a8
dayanch96 <38832025+dayanch96@users.noreply.github.com>
2024-03-14 00:42:16 +0300

    Move bundle to header file

--- Source/Prefs/Localization.h
+++ Source/Prefs/Localization.h
@@ -1,6 +1,18 @@
 #import <Foundation/Foundation.h>
+#import <rootless.h>
 
-extern NSBundle *YTMusicUltimateBundle();
+static inline NSBundle *YTMusicUltimateBundle() {
+    static NSBundle *bundle = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        NSString *tweakBundlePath = [[NSBundle mainBundle] pathForResource:@"YTMusicUltimate" ofType:@"bundle"];
+        if (tweakBundlePath)
+            bundle = [NSBundle bundleWithPath:tweakBundlePath];
+        else
+            bundle = [NSBundle bundleWithPath:ROOT_PATH_NS(@"/Library/Application Support/YTMusicUltimate.bundle")];
+    });
+    return bundle;
+}
 
 static inline NSString *LOC(NSString *key) {
     NSBundle *tweakBundle = YTMusicUltimateBundle();
